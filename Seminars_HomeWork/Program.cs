db5824ce149d990bd8bdfd85edcc47bd1d1f15e4
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-20 23:46:21 +0500

    Решил третью задачу

--- Seminars_HomeWork/Program.cs
+++ Seminars_HomeWork/Program.cs
@@ -18,25 +18,34 @@
 
 //Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 
-Console.WriteLine("Input first number");
-int FirNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input second number");
-int SecNum = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input third number");
-int ThirdNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input first number");
+// int FirNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input second number");
+// int SecNum = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input third number");
+// int ThirdNum = Convert.ToInt32(Console.ReadLine());
 
-if (FirNum < SecNum)
-{
-    if (SecNum < ThirdNum)
-        Console.WriteLine($"Max number is {ThirdNum}");
-    else
-        Console.WriteLine($"Max number is {SecNum}");
-}
-else
-Console.WriteLine($"Max number is {FirNum}");
+// if (FirNum < SecNum)
+// {
+//     if (SecNum < ThirdNum)
+//         Console.WriteLine($"Max number is {ThirdNum}");
+//     else
+//         Console.WriteLine($"Max number is {SecNum}");
+// }
+// else
+// Console.WriteLine($"Max number is {FirNum}");
 
 //Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
 
+Console.WriteLine("Input first number");
+int Num = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+while (count <= Num)
+{
+    if (count % 2 == 0)
+        Console.WriteLine(count);
+    count += 1;
+}
 
 
 //Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
