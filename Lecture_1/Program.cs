a22f8ca9e52c148656d53daf2de77b060f53105a
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-21 21:30:12 +0500

    Задача 2 по сложению двуъ чисел

--- Lecture_1/Program.cs
+++ Lecture_1/Program.cs
@@ -1,5 +1,13 @@
-﻿Console.Write("Input your name ");
-string name = Console.ReadLine ();
-Console.Write("Hello!, ");
-Console.WriteLine(name);
+﻿// Задача 1 по вводу и выводу
 
+// Console.Write("Input your name ");
+// string name = Console.ReadLine ();
+// Console.Write("Hello!, ");
+// Console.WriteLine(name);
+
+// Задача 2. Сложение двух чисел
+
+int numberA = 1;
+int NumberB = 5;
+int result = numberA + NumberB;
+Console.WriteLine(result);
