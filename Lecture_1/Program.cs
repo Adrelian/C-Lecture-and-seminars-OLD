1c5464ba7101d32a959770f4cc725f50f17e5b66
Anatolii <anatolii.polovnikov@gmail.com>
2022-11-21 21:50:55 +0500

    Программа приветствия персонализированная

--- Lecture_1/Program.cs
+++ Lecture_1/Program.cs
@@ -14,9 +14,24 @@
 
 // Задача 3. Деление двух чисел
 
-double numberA = new Random().Next(1,15); // 1 2 3 4 ... 14
-Console.WriteLine(numberA);
-double NumberB = new Random().Next(1,15); // 1 2 3 4 ... 14
-Console.WriteLine(NumberB);
-double result = numberA / NumberB;
-Console.WriteLine(result);
+// double numberA = new Random().Next(1,15); // 1 2 3 4 ... 14
+// Console.WriteLine(numberA);
+// double NumberB = new Random().Next(1,15); // 1 2 3 4 ... 14
+// Console.WriteLine(NumberB);
+// double result = numberA / NumberB;
+// Console.WriteLine(result);
+
+// Задча 4. Персонализированное приветсвие
+
+Console.Write("Input your name ");
+string name = Console.ReadLine(); // пользовательский ввод
+
+if(name.ToLower() == "masha")
+{
+    Console.Write("Great. This is Masha");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(name);
+}
